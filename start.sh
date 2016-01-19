29aa123064418146cf7795a8fc2af454f5fc8d7d
Byron Lutz <byron@factual.com>
2016-01-19 15:05:18 -0800

    Removed unneeded output commands from start script

--- start.sh
+++ start.sh
@@ -1,19 +1,11 @@
-echo "Starting data validation demo server"
-echo "------------------------------------"
-
 touch ~/.ssh/id_rsa
 echo -e "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-echo "Cloning git repo"
 git clone $GIT_REPO repo
 cd repo
 npm install
-
-echo "Running gulp"
-cd repo
 ./node_modules/.bin/gulp build
 
-echo "Starting web server"
 npm install -g http-server
 http-server dist
